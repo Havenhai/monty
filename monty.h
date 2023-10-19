0bc41af8f04a995650a3ccb1a994edbbdba3f437
Kirubiel Beza <kirubelkeb@gmail.com>
2023-10-19 04:22:47 -0700

    monty

--- monty.h
+++ monty.h
@@ -10,33 +10,34 @@
 #include <sys/types.h>
 
 /**
- * struct instruction_s - struct opcode & function
- * @opcode: opcode
- * @f: handle the opcode
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
  *
- * Description: function & opcode
- * for LIFO, FIFO, stack, queues
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-/**
- * struct stack_s - struct doubly linked list representation of a stack
- * @n: int
- * @prev: previous element of the stack or queue
- * @next: next element of the stack or queue
- *
- * Description: doubly linked list
- * for stack, queues, LIFO, FIFO
- */
-typedef struct stack_s
-{
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-} stack_t;
+
 /**
  * struct bus_s - struct variables
  * @arg: val
@@ -45,6 +46,7 @@ typedef struct stack_s
  * @lifi: flag change
  * Description: values
  */
+
 typedef struct bus_s
 {
 	char *arg;
